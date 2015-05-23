54fe6671e07f39410751a30fceb9fb59a4edb1a6
Ranga Vadhineni <vrangarao@gmail.com>
2015-05-23 15:02:00 +0100

    Third tab added for test

---
+++ StatusIndicator/ThirdTabController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace StatusIndicator
+{
+	partial class ThirdTabController : UIViewController
+	{
+		public ThirdTabController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
